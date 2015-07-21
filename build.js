90817fd2d9771fdc2c2fba874f23db4c787e6ac1
Kliment Mamykin <kmamyk@gmail.com>
2015-07-21 00:34:27 -0400

    Added links to read/steal metalsmith config

--- build.js
+++ build.js
@@ -1,3 +1,9 @@
+// https://www.doxiq.com/posts/Hacking-Lead-Generation-How-doxIQ-built-a-lightweight-but-effective-marketing-stack/
+// http://keetology.com/blog/rebuilding-keetology
+// https://github.com/christophercliff/christophercliff.github.io/blob/supermaster/build.js
+// https://github.com/X1011/git-directory-deploy
+// https://webxcerpt.github.io/openCPQ/blog-posts/2015-05-21-website-toolchain.html
+
 var Metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
 //var metadata = require('metalsmith-metadata');
