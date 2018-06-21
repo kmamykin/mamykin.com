4ff5dcd912b6d992b50ca9dd1dc03340d8d2af07
Kliment Mamykin <kmamyk@gmail.com>
2018-06-21 15:55:28 -0400

    Added some site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: 'Kliment Mamykin',
+    siteUrl: 'https://www.mamykin.com',
+    description: 'Personal site',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
