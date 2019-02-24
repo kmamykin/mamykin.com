bc2dca3e687fe81bbc059eca13cde5394f31d8a7
Kliment Mamykin <kmamyk@gmail.com>
2019-02-24 18:47:30 -0500

    Added blank about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+
+export default () => (
+  <Layout>
+    <div>
+      <h1>About Gatsby</h1>
+      <p>Such wow. Very React.</p>
+    </div>
+  </Layout>
+)
