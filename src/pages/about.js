f1e0c1538db7259a0ded7d92757224eeb39cb1b7
Kliment Mamykin <kmamyk@gmail.com>
2019-02-24 22:21:44 -0500

    Added typography.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,9 @@
 import React from "react"
 import Layout from "../components/layout"
 
-
 export default () => (
   <Layout>
-    <div>
-      <h1>About Gatsby</h1>
-      <p>Such wow. Very React.</p>
-    </div>
+    <h1>About Gatsby</h1>
+    <p>Such wow. Very React.</p>
   </Layout>
 )
