b905d03e97da67721930ea21232da781434051d3
Kliment Mamykin <kmamyk@gmail.com>
2019-04-21 01:08:52 -0400

    Moved posts to blog.mamykin.com

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/posts/`,
+        path: `${__dirname}/../blog.mamykin.com/posts/`,
         ignore: [`**/\.*`], // ignore files starting with a dot
       },
     },
